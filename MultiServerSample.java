3bd2b7ed82208f993ec956f54875297a82959eae
Gimlet <45482966+nanoGimlet@users.noreply.github.com>
2019-05-19 12:16:43 +0900

    Sample1

---
+++ MultiServerSample.java
@@ -0,0 +1,90 @@
+//複数接続Socket通信サンプルプログラム(サーバー)
+//クライアントからの接続を待ち、接続が行なわれたら
+//1行のデータを受け取り、コンソールに表示して接続を切断する。
+//複数のクライアントとの通信をスレッドにより行なう。
+//プログラム終了は，コマンドプロンプトでCTRL-C
+import java.net.*;
+import java.io.*;
+
+class MultiServerSample 
+{
+  public static void main(String[] args){
+      ServerSocket serverSoc = null;
+      try {
+        // ポート番号は、30000
+        //ソケットを作成
+        serverSoc = new ServerSocket(30000);
+        boolean flag=true;
+      
+        //クライアントからの接続を待機するaccept()メソッド。
+        //accept()は、接続があるまで処理はブロックされる。
+        //もし、複数のクライアントからの接続を受け付けるようにするには
+        //スレッドを使う。
+        //accept()は接続時に新たなsocketを返す。これを使って通信を行なう。
+        System.out.println("Waiting for Connection. ");
+        while(flag){
+          Socket socket=null;
+          socket = serverSoc.accept();
+          //accept()は、クライアントからの接続要求があるまでブロックする。
+          //接続があれば次の命令に移る。
+          //スレッドを起動し、クライアントと通信する。
+          new SrvThread(socket).start();
+          System.out.println("Waiting for New Connection. ");
+        }
+      }
+      catch (IOException e) {
+        System.out.println("IOException!");
+        e.printStackTrace();
+      }
+      finally{
+        try{
+          if (serverSoc != null){
+            serverSoc.close();
+          }
+        }
+        catch (IOException ioex) {
+          ioex.printStackTrace();
+        }
+      }
+   }
+}
+
+class SrvThread extends Thread{
+  private Socket soc;
+
+  public SrvThread(Socket sct){
+    soc=sct;
+    System.out.println("Thread is Generated.  Connect to " +
+        soc.getInetAddress());
+  }
+
+  public void run(){
+    try{
+      //socketからのデータはInputStreamReaderに送り、さらに
+      //BufferedReaderによってバッファリングする。
+      BufferedReader reader = new BufferedReader
+        (new InputStreamReader(soc.getInputStream()));
+      //Clientへの出力用PrintWriter
+      PrintWriter sendout = new PrintWriter(soc.getOutputStream(), true);
+      //データ読み取りと表示。
+      String line;
+      line = reader.readLine();
+      System.out.println("Message from client :" + line);
+      //Clientにメッセージ送信
+      sendout.println("Message is received.");
+    }
+    catch(IOException ioex){
+      ioex.printStackTrace();
+    }
+    finally{
+      try{
+        if(soc != null){
+          soc.close();
+        }
+      }
+      catch (IOException ioex){
+        ioex.printStackTrace();
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ソフトウェア制作用
+
+## 授業のメモ
+[第１回](./softmemo.txt)
+
+## 複数のコマンドプロンプトからの送受信可能なプログラム例
+[サーバー：MultiServerSample.java](./MultiServerSample.java)
+[クライアント：MultiClientSample.java](./MultiClientSample.java)
+
+・“java MultiServerSample 30000”でポート番号を指定
+・複数のコマンドプロンプトから“java MultiClientSample.java localhost "好きな文字"”でメッセージを送信
