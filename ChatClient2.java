1d3fe8ec99edc3497cd05e8caa4164c48e0bfe05
Tommy <tommy@Tommy86.local>
2019-06-10 17:38:48 +0900

    6/10

---
+++ ChatClient2.java
@@ -0,0 +1,31 @@
+//サーバーから送られてきた文字の表示専門のクラス
+
+
+import java.io.*;
+import java.net.*;
+
+public class ChatClient2 extends Thread {
+    Socket socket;
+    public ChatClient2(Socket socket){
+        this.socket=socket;
+        this.start();
+    }
+    
+    public void run(){
+        try{
+            while(true){
+            ReaderWriter reader = new ReaderWriter(socket);
+            String ReadMessages = reader.in.readLine();
+            System.out.println(ReadMessages);  
+            }
+        }  
+        catch(IOException e){
+            System.out.println("closing...");
+            try{
+            socket.close();
+            }catch(Exception a){
+
+            }
+        }
+    }
+}
