3b6178d64d799ad8eb74e02d30dea095156332f2
Ryogo-san <rito@akane.waseda.jp>
2019-06-22 16:21:26 +0900

    β版ofβ版。PORT入力とCUIの対応確認

--- Ryogo_GUI/Mainapp.java
+++ Ryogo_GUI/Mainapp.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import java.io.IOException; //
+import java.net.InetAddress; //
+import java.net.Socket; //
+import java.util.*; //
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
@@ -6,20 +10,50 @@ import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 public class Mainapp extends Application{
-
+    public static int PORT; //
     @Override
     public void start(Stage primaryStage) throws Exception {
 
 
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("app2.fxml"));
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("portConnect.fxml"));
 
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+        /*
+        String server = InetAddress.getLocalHost().getHostAddress();
+        //Scanner scp = new Scanner(System.in);
+        PORT = portController.getPort();
+        System.out.println("PORT:"+PORT);
+        Socket socket = null;
+        try {
+            InetAddress addr = InetAddress.getByName(server);
+            socket = new Socket(addr, PORT);
+            Connect con = new Connect();
+            con.connect(socket);
+        } catch (IOException e) {
+
+        }
+        */
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         launch(args);
+        //
+        String server = InetAddress.getLocalHost().getHostAddress();
+        //Scanner scp = new Scanner(System.in);
+        PORT = portController.getPort();
+        System.out.println("port:"+PORT);
+        Socket socket = null;
+        try {
+            InetAddress addr = InetAddress.getByName(server);
+            socket = new Socket(addr, PORT);
+            Connect con = new Connect();
+            con.connect(socket);
+        } catch (IOException e) {
+
+        }
+       //
     }
 }
