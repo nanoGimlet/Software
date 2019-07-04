186abb60703eca07e64bb76fa3f8077b767b5604
Ryogo-san <rito@akane.waseda.jp>
2019-07-04 20:48:37 +0900

    頑張りましょう

---
+++ gimletLast/RoomSizeOver.java
@@ -0,0 +1,21 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+public class RoomSizeOver {
+
+    @FXML
+    private ResourceBundle resources;
+
+    // @FXML
+    // private Button roomsizebutton;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
